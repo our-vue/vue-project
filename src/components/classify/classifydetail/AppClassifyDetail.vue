42b8af0a32b57db02d896c8cb692d3c86c892152
yiyu11 <1336921038@qq.com>
2017-10-18 21:07:23 +0800

    b

--- src/components/classify/classifydetail/AppClassifyDetail.vue
+++ src/components/classify/classifydetail/AppClassifyDetail.vue
@@ -11,7 +11,8 @@
 			</div>
 			<div class="swiper-container">
 				<div class="swiper-wrapper">
-					<div class="swiper-slide" v-for="food in foods" :key="food.id">{{food.name}}</div>
+					<div class="swiper-slide" v-for="food in foods" :key="food.id" style="min-width: 100px;" @click="type=food.id">{{food.name}}
+					</div>
 				</div>
 			</div>
 		</div>
@@ -22,12 +23,11 @@
 	//http://api5.wochu.cn/api/goods/getcategoryandgoods?orderId=1&categoryId=2
 	import { mapState, mapMutations, mapActions, mapGetters } from 'vuex'
 	import Swiper from '../../../../static/base/swiper.min.js'
-	console.log(Swiper)
 	export default {
 		name: 'AppClassifyDetail',
 		data() {
 			return {
-
+				 	type:1
 			}
 		},
 		computed: {
@@ -40,7 +40,7 @@
 				
 		},
 		methods: {
-			backTo() {
+			backTo(){
 				this.$emit('goback')
 			}
 		},
@@ -48,18 +48,17 @@
 			this.$on('goback', function() {
 				window.history.go(-1)
 			})
-//			let swiper = new Swiper('.swiper-container', {
-//				slidesPerView: 4,
-//				paginationClickable: true,
-//				spaceBetween: 30
-//			});
-
+			let swiper = new Swiper('.swiper-container', {			
+//      slidesPerView: 3,
+        paginationClickable: true,
+			});
 		}
 	}
 </script>
- @import '../../../../static/base/swiper-3.4.2.min.css' 
-<style scoped lang="scss">
+ @import '../../../../static/base/swiper-3.4.2.min.css'
 
+<style lang="scss">
+ 
 	.app-classify-detail {
 		.header {
 			background: #fff;
@@ -77,11 +76,11 @@
 				}
 				p {
 					font-size: 0.2rem;
-					span {
+				span {
 						font-size: 0.26rem;
 						color: #333;
 						margin-right: 0.1rem;
-						img {
+					img {
 							width: 0.25rem;
 							height: 0.25rem;
 						}
@@ -93,19 +92,21 @@
 			}
 			.swiper-container {
 				width: 100%;
-				height: 0.5rem;
-				.swiper-wraper{
-					display:flex;
-						.swiper-slide {
-								text-align: center;
-								font-size: 18px;
-								background: #fff;
-								display: flex;
-								justify-content: center;
-								align-items: center;
-					}
-				}
-				
+				height: 0.5rem;			
+				.swiper-wrapper {
+					width: 100%;
+					height: 100%;
+					display:flex;	
+					align-items:center;	
+					.swiper-slide {
+						 padding:0 0.1rem;
+						 color:#666;
+					}		
+					.on{
+						 color:#008000;
+						 border-bottom:1px solid #008000;
+					}	
+				}				
 			}
 		}
 	}
